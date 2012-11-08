858483e802e8eddce8888ec7c123ac16865130d7
abhis07 <a.bhandari075@gmail.com>
2012-11-08 16:05:06 +0545

    changed ui of the add joke page
    remove php

--- application/views/footer.php
+++
@@ -1,5 +0,0 @@
-<p class="footer">		<a href="<?php echo base_url('index.php/kushaljokes/add')?>">Add Jokes</a> | <a href="<?php echo base_url('index.php/kushaljokes/all')?>">List Jokes</a></p>
-</div>
-
-</body>
-</html>
